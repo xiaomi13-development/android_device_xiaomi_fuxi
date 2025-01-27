575d56cdf106f83744e350a057a23692e4adfadb
xiaomi13-development <andrey.denikin17@gmail.com>
2025-01-27 20:31:51 +0300

    fuxi: Add EverestOS flags
    Signed-off-by: xt0032rus <andrey.denikin8@gmail.com>

--- lineage_fuxi.mk
+++ lineage_fuxi.mk
@@ -14,10 +14,28 @@ $(call inherit-product, device/xiaomi/fuxi/device.mk)
 # Inherit LineageOS configurations
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device identifier
 PRODUCT_DEVICE := fuxi
 PRODUCT_NAME := lineage_fuxi
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := 2211133C
+PRODUCT_MODEL := Xiaomi 13
 PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_SYSTEM_NAME := Xiaomi 13
+PRODUCT_SYSTEM_DEVICE := Xiaomi 13
+
+# EverestOS official flags
+EVEREST_MAINTAINER := ♦️xt0032rus♦️
+EVEREST_BUILD_TYPE := OFFICIAL
+WITH_GMS := true
+
+# Rom flags
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# UDFPS
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
 
 BUILD_FINGERPRINT := Xiaomi/fuxi/fuxi:15/AQ3A.240912.001/OS2.0.3.0.VMCCNXM:user/release-keys
